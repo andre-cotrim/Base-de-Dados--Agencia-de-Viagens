cf2dde8790fb8b156517c8ee912f81c3ce4343d2
andre-cotrim <andrecotrim2005@gmail.com>
2024-11-27 14:46:30 +0000

    fixed foreign keys

--- projeto.sql
+++ projeto.sql
@@ -1,34 +1,35 @@
 CREATE TABLE PASSAGEIRO(
-    Número_CC TEXT PRIMARY KEY,
+    Numero_CC TEXT PRIMARY KEY,
     Nome TEXT NOT NULL, 
     Data_de_Nascimento DATE NOT NULL, 
-    Estado_de_Vacinação BOOLEAN NOT NULL,
+    Check_Vacinacao_Necessaria BOOLEAN NOT NULL,
     ID_Passaporte INT NOT NULL
+    CHECK ((JULIANDAY('now') - JULIANDAY(Data_de_Nascimento)) / 365 >= 18)
 );
+SELECT Numero_CC, Data_de Nascimento, (JULIANDAY('now')-JULIANDAY(Data_de_Nascimento)) / 365 AS Idade FROM PASSASGEIRO;
 
---o chatgpt diz que essa função nao esta disponivel no sqlite
-SELECT Número_CC, Data_de_Nascimento, DATEDIFF(YY, Data_de_Nascimento, GETDATE()) as IDADE FROM PASSAGEIRO; 
--- seria->  SELECT Número_CC, Data_de Nascimento, (JULIANDAY('now')-JULIANDAY(Data_de_Nascimento)) / 365 AS IDADE FROM PASSASGEIRO;
-
---restrigir text
 --perguntar stor como fazer IDADE
 --confirmar booleans
 --unique
+--constrain
+--nulls
+--drop table
 
 CREATE TABLE CLIENTE(
-    Número_CC INT PRIMARY KEY,
+    Numero_CC INT PRIMARY KEY,
     IBAN TEXT,
     NIF TEXT,
     Email TEXT NOT NULL,
-    Número_de_Telefone TEXT NOT NULL,
+    Numero_de_Telefone TEXT NOT NULL,
     Morada_de_Faturação TEXT ,
-    FOREIGN KEY (Número_CC) REFERENCES PASSAGEIRO(NÚMERO_CC)
+    FOREIGN KEY (Numero_CC) REFERENCES PASSAGEIRO(Numero_CC)
 );
+CHECK (Idade>=18);
 
-CREATE TABLE AVIÃO(
-    ID_Avião INT PRIMARY KEY,
+CREATE TABLE AVIAO(
+    ID_Aviao INT PRIMARY KEY,
     Modelo TEXT ,
-    Companhia_Aérea TEXT NOT NULL,
+    Companhia_Aerea TEXT NOT NULL,
     Capacidade INT NOT NULL,
     Ano_de_Produção INT NOT NULL
 );
@@ -36,34 +37,31 @@ CREATE TABLE AVIÃO(
 
 CREATE TABLE RESERVA(
     ID_da_Reserva INT PRIMARY KEY,
-    Data_de_Iníco DATE NOT NULL,
+    Data_de_Inico DATE NOT NULL,
     Data_de_Fim DATE NOT NULL,
-    Bagagem TEXT, --ver tipo aqui
-    ID_da_Rota INT REFERENCES VIAGEM(ID_da_Rota) NOT NULL
-    
+    Bagagem INT,
+    ID_da_Rota INT NOT NULL,
+    FOREIGN KEY (ID_da_Rota) REFERENCES VIAGEM(ID_da_Rota)
 );
 
---nao sei se tem que se por na tabela ou não
-SELECT Data_de_Início, Data_De_Fim DATEDIFF(DD, Data_de_Início, Data_de_Fim) as Dias FROM RESERVA;
--- seria -> SELECT ID_da_Reserva, Data_de_Inicio,Data_de_Fim, (JULIANDAY(Data_de_Fim)-JULIANDAY(Data_de_Inicio)) AS DAYS FROM RESERVA;
-
-
+SELECT ID_da_Reserva, Data_de_Inicio,Data_de_Fim, (JULIANDAY(Data_de_Fim)-JULIANDAY(Data_de_Inicio)) AS DAYS FROM RESERVA;
 
 --quartos disponiveis?
 CREATE TABLE ALOJAMENTO(
     Endereço TEXT PRIMARY KEY,
     Check-in DATE NOT NULL,
     Check-out DATE NOT NULL,
-    Número_de_Quartos INT,
-    Número_de_Telefone INT,
-    Avaliação FLOAT
+    Numero_de_Quartos INT,
+    Numero_de_Telefone INT,
+    Avaliaçao FLOAT
 );
 
 CREATE TABLE TOUR(
     Programa TEXT PRIMARY KEY,
-    Duração INT,
+    Duracao INT,
     Idioma TEXT,
-    Cidade TEXT REFERENCES CIDADE(Nome_da_Cidade)
+    Cidade TEXT,
+    FOREIGN KEY (Cidade) REFERENCES CIDADE(Nome_da_Cidade)
 );
 
 --não sei derivar idioma || Pedro: eu vi no chatGPT como se faz mas nao tenho a certeza se demos nas aulas
@@ -71,16 +69,19 @@ CREATE TABLE TOUR(
 
 CREATE TABLE VIAGEM(
     ID_da_Rota INT PRIMARY KEY,
-    Hora_de_Embarque TIME,
-    Hora_de_Chegada TIME,
-    Cidade_de_Embarque TEXT REFERENCES CIDADE(Nome_da_Cidade),
-    Cidade_de_Chegada TEXT REFERENCES CIDADE(Nome_da_Cidade),
+    Hora_de_Embarque TIME NOT NULL,
+    Hora_de_Chegada TIME NOT NULL,
+    Cidade_de_Embarque TEXT NOT NULL,
+    Cidade_de_Chegada TEXT NOT NULL,
+    FOREIGN KEY (Cidade_de_Embarque) REFERENCES CIDADE(Nome_da_Cidade),
+    FOREIGN KEY (Cidade_de_Chegada) REFERENCES CIDADE(Nome_da_Cidade)
 );
 
 CREATE TABLE AEROPORTO (
     Nome_do_Aeroporto TEXT PRIMARY KEY,
-    Porta_de_Embarque INT,
-    Nome_Cidade TEXT REFERENCES Cidade(Nome_da_Cidade),
+    Porta_de_Embarque INT NOT NULL,
+    Cidade TEXT NOT NULL,
+    FOREIGN KEY (Cidade) REFERENCES Cidade(Nome_da_Cidade)
 );
 
 CREATE TABLE CIDADE (
@@ -89,22 +90,12 @@ CREATE TABLE CIDADE (
     País TEXT
 ):
 
-CREATE TABLE ESCALA(
-    ID_da_Rota TEXT REFERENCES VIAGEM(ID_da_Rota) PRIMARY KEY,
-    Nome_do_Aeroporto TEXT REFERENCES AEROPORTO(Nome_do_Aeroporto) PRIMARY KEY, --pedro vai te fuder
-    Hora_de_Chegada TIME,
-    Hora_de_Partida TIME
+SELECT (STRFTIME('%s', Hora_de_Partida) - STRFTIME('%s', Hora_de_Chegada)) / 60 AS Tempo_de_Espera FROM ESCALA;
 
-);
-
---PROBLEMA IGUAL AO ANTERIOR 
-SELECT Hora_de_Partida, Hora_de_Chegada DATEDIFF(DD, Hora_de_Chegada, Hora_de_Partida) as Tempo_de_Espera FROM ESCALA;
---seria -> SELECT (STRFTIME('%s', Hora_de_Partida) - STRFTIME('%s', Hora_de_Chegada)) / 60 AS Tempo_de_Espera FROM ESCALA;
-
-CREATE TABLE FUNCIONÁRIO(
+CREATE TABLE FUNCIONARIO(
     ID_Empresa INT PRIMARY KEY,
     Anos_de_Serviço INT NOT NULL,
-    Número_de_Telefone Text NOT NULL,
+    Numero_de_Telefone Text NOT NULL,
     Nome TEXT NOT NULL,
     NIF TEXT NOT NULL,
     IBAN TEXT NOT NULL,
@@ -113,27 +104,28 @@ CREATE TABLE FUNCIONÁRIO(
 );
 
 CREATE TABLE GUIA(
-    ID_Empresa INT REFERENCES FUNCIONÁRIO(ID_Empresa) PRIMARY KEY,
+    ID_Empresa INT,
     Idioma TEXT NOT NULL,
     ID_Diploma_de_Turismo INT PRIMARY KEY
-    
+    FOREIGN KEY (ID_Empresa) REFERENCES FUNCIONARIO(ID_Empresa)
 );
 
 CREATE TABLE HOSPEDEIRO(
-    ID_Empresa INT REFERENCES FUNCIONÁRIO(ID_Empresa) PRIMARY KEY,
-    ID_Certidão_De_Formação INT PRIMARY KEY
-    
+    ID_Empresa INT PRIMARY KEY,
+    ID_Certidão_De_Formação INT PRIMARY KEY,
+    FOREIGN KEY (ID_Empresa) REFERENCES FUNCIONARIO(ID_Empresa)
 );
 
 CREATE TABLE PILOTO(
     ID_Empresa INT REFERENCES FUNCIONÁRIO(ID_Empresa) PRIMARY KEY,
     ID_Licensa INT PRIMARY KEY,
-    
+    FOREIGN KEY (ID_Empresa) REFERENCES FUNCIONARIO(ID_Empresa)
 );
 
 CREATE TABLE PNR(
-    Número_CC TEXT REFERENCES PASSAGEIRO(Número_CC) PRIMARY KEY,
-    ID_da_Reserva INT REFERENCES RESERVA(ID_da_Reserva)
+    Numero_CC TEXT,
+    ID_da_Reserva INT REFERENCES RESERVA(ID_da_Reserva),
+    FOREIGN KEY (ID_Empresa) REFERENCES FUNCIONARIO(ID_Empresa)
 );
 
 CREATE TABLE AQUISIÇÃO(){
@@ -141,7 +133,8 @@ CREATE TABLE AQUISIÇÃO(){
     ID_da_Reserva INT REFERENCES RESERVA(ID_da_Reserva) NOT NULL,
     Data_de_Pagamento DATE,
     Valor_Pago INT CHECK(Valor_Pago>0) NOT NULL,
-    Estado_de_Pagamento BOOLEAN NOT NULL
+    Estado_de_Pagamento BOOLEAN NOT NULL,
+
 };
 
 CREATE TABLE RESERVA_DE_ALOJAMENTO(){
@@ -156,7 +149,7 @@ CREATE TABLE ITINERÁRIO(){
 
 CREATE TABLE VOO(){
     ID_da_Rota INT REFERENCES VIAGEM(ID_da_Rota) PRIMARY KEY, 
-    ID_do_Avião INT REFERENCES AVIÃO(ID_do_Avião) NOT NULL
+    ID_do_Avião INT REFERENCES AVIAO(ID_do_Aviao) NOT NULL
 };
 
 
@@ -166,28 +159,42 @@ CREATE TABLE RESERVA_DE_TOUR(){
 };
 
 CREATE TABLE ROTA(){
-    ID_da_Rota INT REFERENCES VIAGEM(ID_da_Rota) PRIMARY KEY,
-    Nome_do_Aeroporto TEXT REFERENCES AEROPORTO(Nome_do_Aeroporto) PRIMARY KEY,
+    ID_da_Rota INT,
+    Aeroporto_Partida TEXT,
+    Aeroporto_Chegada TEXT,
     Hora_de_Chegada TIME NOT NULL,
-    Hora_de_Partida TIME NOT NULL
+    Hora_de_Partida TIME NOT NULL,
+    PRIMARY KEY (ID_da_Rota,Aeroporto_Partida,Aeroporto_Chegada),
+    FOREIGN KEY (ID_da_Rota) REFERENCES VIAGEM(ID_da_Rota),
+    FOREIGN KEY (Aeroporto_Chegada) REFERENCES AEROPORTO(Nome_do_Aeroporto),
+    FOREIGN KEY (Aeroporto_Partida) REFERENCES AEROPORTO(Nome_do_Aeroporto)
 };
-
 --FALTA CALCULAR O TEMPO DE ESPERA NA ROTA
 --seria SELECT (STRFTIME('%s', Hora_de_Chegada) - STRFTIME('%s', Hora_de_Partida)) / 60 AS Tempo_de_Espera FROM ROTA;
 
 CREATE TABLE ORIENTAÇÃO(){
-    Programa TEXT REFERENCES ROTA(ID_da_Rota) PRIMARY KEY,
-    ID_Diploma_de_Turismo INT REFERENCES GUIA(ID_Diploma_de_Turismo)PRIMARY KEY
+    Programa TEXT,
+    ID_Diploma_de_Turismo INT,
+    PRIMARY KEY (Programa, ID_Diploma_de_Turismo),
+    FOREIGN KEY (Programa) REFERENCES TOUR(ID_da_Rota),
+    FOREIGN KEY (ID_Diploma_de_Turismo) REFERENCES GUIA(ID_Diploma_de_Turismo)
+    
 };
 
 CREATE TABLE EQUIPA_DE_BORDO_HOSPEDEIRO(){
     ID_da_Rota INT REFERENCES VIAGEM(ID_da_Rota) PRIMARY KEY,
     ID_Certidão_De_Formação INT REFERENCES HOSPEDEIRO(ID_Certidão_De_Formação) PRIMARY KEY,
+    PRIMARY KEY (ID_da_Rota,ID_Certidão_De_Formação),
+    FOREIGN KEY (ID_da_Rota) REFERENCES VIAGEM(ID_da_Rota),
+    FOREIGN KEY (ID_Certidao_De_Formaçao) REFERENCES GUIA(ID_Certidao_De_Formação),
 };
 
 CREATE TABLE EQUIPA_DE_BORDO_PILOTO(){
-    ID_da_Rota INT REFERENCES VIAGEM(ID_da_Rota) PRIMARY KEY,
-    ID_Licensa INT REFERENCES PILOTO(ID_Licensa) PRIMARY KEY
+    ID_da_Rota INT,
+    ID_Licensa INT,
+    PRIMARY KEY (ID_da_Rota,ID_Licensa),
+    FOREIGN KEY (ID_da_Rota) REFERENCES VIAGEM(ID_da_Rota),
+    FOREIGN KEY (ID_Licensa) REFERENCES PILOTO(ID_Licensa)
 };
 
 --ver isto do pagamento
@@ -196,7 +203,9 @@ CREATE TABLE PAGAMENTO(
     Data_de_Pagamento DATE,
     Valor_Pago INT CHECK (Valor_Pago>0),
     ESTADO_DE_PAGAMENTO BOOLEAN NOT NULL
-    Número_CC TEXT REFERENCES CLIENTE(NÚMERO_CC),
-    ID_da_Reserva TEXT REFERENCES RESERVA(ID_da_Reserva)
+    Número_CC TEXT,
+    ID_da_Reserva TEXT,
+    FOREIGN KEY (Numero_CC) REFERENCES CLIENTE(NÚMERO_CC),
+    FOREIGN KEY (ID_da_Reserva) REFERENCES  RESERVA(ID_da_Reserva)
 );
 
